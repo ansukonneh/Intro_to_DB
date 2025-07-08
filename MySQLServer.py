88ea14cee9db69e2c83e50e207b3ac13308957a3
Ansu Konneh <ansukonneh028@gmail.com>
2025-07-08 02:26:47 +0000

    fixing error

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,9 +1,24 @@
 import mysql.connector
-mydb = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="Mayking@3",
-    database="alx_book_store"
-)
+from mysql.connector import Error
 
-print("Alx database is printed", mydb.get_server_info())
+try:
+    # Connect to MySQL server (no DB specified yet)
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="Mayking@3"
+    )
+
+    if mydb.is_connected():
+        mycursor = mydb.cursor()
+        mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+        print("Alx database is printed", mydb.get_server_info())
+
+except Error as e:
+    print(" Error:", e)
+
+finally:
+    if 'mycursor' in locals():
+        mycursor.close()
+    if 'mydb' in locals() and mydb.is_connected():
+        mydb.close()
--- alx_book_store.sql
+++ alx_book_store.sql
@@ -19,5 +19,6 @@ order_date DATE);
 CREATE TABLE Order_Details(orderdetailid INT PRIMARY KEY,
 order_id INT,
 book_id INT,
- FOREIGN KEY (order_id) REFERENCES Orders(order_id), FOREIGN KEY (book_id) REFERENCES Books (book_id), 
- quantity DOUBLE);
+quantity DOUBLE,
+ FOREIGN KEY (order_id) REFERENCES Orders(order_id), FOREIGN KEY (book_id) REFERENCES Books(book_id), 
+);
